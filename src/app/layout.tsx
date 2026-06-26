28987c2dc6d83b0b82a9101f9e7f04fc90ebef8a
unknown <nqnhu6124@gmail.com>
2026-06-26 13:38:13 +0700

    gtm, index, error valid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { LayoutWrapper } from "@/components/layout/LayoutWrapper";
 import { Providers } from "@/components/providers/Providers";
-import {
-  GoogleTagManagerScript,
-  GoogleTagManagerNoScript,
-} from "@/components/analytics/GoogleTagManager";
+import { GoogleTagManagerNoScript } from "@/components/analytics/GoogleTagManager";
+
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,7 +28,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi" data-scroll-behavior="smooth" className={`${inter.variable} h-full antialiased`}>
-      <GoogleTagManagerScript />
+      <head>
+        {GTM_ID && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
+            }}
+          />
+        )}
+      </head>
       <body className="min-h-full flex flex-col">
         <GoogleTagManagerNoScript />
         <Providers>
